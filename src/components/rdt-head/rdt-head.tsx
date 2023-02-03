cb0ec37ee50710443dfa9ed1eafc7c1f26464e76
sr <waldnerds@gmail.com>
2023-02-03 18:47:42 +0100

    header tests

--- src/components/rdt-head/rdt-head.tsx
+++ src/components/rdt-head/rdt-head.tsx
@@ -1,27 +1,37 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, getAssetPath, Host, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'rdt-head',
   styleUrl: 'rdt-head.scss',
+  assetsDirs: ['assets']
 })
 export class RdtHead {
 
+  @Prop() logoImage = "logo.png";
+
   render() {
+
     return (
       <Host>
         <div class="header-main">
           <div class="header-container container">
             <div class="header-wrap">
-              <rdt-logo></rdt-logo>
+              <div class="header-logo header-logo-ls logo animated" data-animate="fadeInDown"
+                data-delay=".65">
+                <a href="./" class="logo-link">
+                  <img class="logo-light" src={getAssetPath(`./assets/${this.logoImage}`)}
+                    alt="logo"></img>
+                </a>
+              </div>
               <div class="header-nav-toggle">
-                <a href="#" class="navbar-toggle" data-menu-toggle="header-navbar-menu">
+                <a href="#" class="navbar-toggle" data-menu-toggle="rdt-navbar-menu">
                   <div class="toggle-line">
                     <span></span>
                   </div>
                 </a>
               </div>
               <div class="header-navbar header-navbar-s3">
-                <nav class="header-menu justify-content-between" id="header-navbar-menu">
+                <nav class="header-menu justify-content-between" id="rdt-navbar-menu">
                   <ul class="menu menu-s2 animated" data-animate="fadeInDown" data-delay=".75">
                     <li class="menu-item"><a class="menu-link nav-link" href="#ico">What is ICO</a></li>
                     <li class="menu-item"><a class="menu-link nav-link" href="#tokens">Tokens</a></li>
@@ -32,14 +42,21 @@ export class RdtHead {
                     <li class="menu-item"><a class="menu-link nav-link" href="#contact">Contact</a></li>
                   </ul>
                   <ul class="menu-btns animated" data-animate="fadeInDown" data-delay=".85">
-                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#register-popup" class="btn btn-rg btn-auto btn-outline btn-grad on-bg-theme-dark-alt"><span>Sign Up</span></a></li>
-                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#login-popup" class="btn btn-rg btn-auto btn-outline btn-grad on-bg-theme-dark-alt"><span>Log In</span></a></li>
+                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#register-popup"
+                      class="btn btn-rg btn-auto btn-outline btn-grad on-bg-theme-dark-alt"><span>Sign
+                        Up</span></a></li>
+                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#login-popup"
+                      class="btn btn-rg btn-auto btn-outline btn-grad on-bg-theme-dark-alt"><span>Log
+                        In</span></a></li>
                   </ul>
                 </nav>
               </div>
             </div>
           </div>
         </div>
+        <script src="assets/js/jquery.bundle.js?ver=210"></script>
+        <script src="assets/js/scripts.js?ver=210"></script>
+        <script src="assets/js/charts.js?ver=210"></script>
       </Host>
     );
   }
