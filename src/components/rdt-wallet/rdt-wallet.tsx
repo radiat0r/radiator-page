d8ec5d20f6fe37a61da9e24bff43a0c0617fcb2c
sr <waldnerds@gmail.com>
2023-02-07 22:50:21 +0100

    first table in wallet

--- src/components/rdt-wallet/rdt-wallet.tsx
+++ src/components/rdt-wallet/rdt-wallet.tsx
@@ -9,8 +9,53 @@ export class RdtWallet {
   render() {
     return (
       <Host>
-        <p>Wallet component</p>
-      </Host>
+        <div class="feature feature-center card card-lg-y card-s4">
+          <div class="row justify-content-center">
+            <div class="col-sm-10">
+              <div class="feature-text">
+                <h5 class="title title-md pb-2">Check if you can benefit from our CASHBACK campaigns</h5>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-sm-7">
+              <div class="field-item">
+                <div class="field-wrap">
+                  <input type="text" class="input-bordered" placeholder="Wallet Address"></input>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <button type="submit" class="btn btn-md btn-grad">Check</button>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-sm-10">
+              <div class="data-table-area">
+                <table class="data-table">
+                  <thead class="bg-primary">
+                    <tr class="data-head">
+                      <th class="data-col text-start">Cashback Campaign</th>
+                      <th class="data-col text-center">Benefit</th>
+                    </tr>
+                  </thead>
+                  <tbody class="text-center">
+                    <tr class="data-item">
+                      <td class="data-col-inner index"> Radorables </td>
+                      <td class="data-col-inner alphabetical">
+                        <div class="d align-items-center">
+                          <em class="icon-bg icon-bg-md icon-bg-eth ikon ikon-eth"></em>
+                          <h4 class="title title-sm title-dark">Ethereum (ETH)</h4>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Host >
     );
   }
 
