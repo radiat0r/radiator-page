c215b7202ba6107fd817a0d321471bda42b27470
sr <waldnerds@gmail.com>
2023-02-16 22:07:52 +0100

    add logo png

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,5 @@
 import { Config } from '@stencil/core';
-import {sass} from "@stencil/sass";
+import { sass } from "@stencil/sass";
 
 export const config: Config = {
   namespace: 'ui-stencil',
@@ -8,8 +8,19 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    {
+      type: 'dist-custom-elements',
+      copy: [
+        {
+          // this copies the logo.png into the root of the www folder
+          src: './*.{jpg,png}', 
+          dest: 'www',
+          warn: true,
+        }
+      ]
+    },
   ],
-  plugins:[
+  plugins: [
     sass()
   ]
 };
