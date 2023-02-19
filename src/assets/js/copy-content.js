80b73492a9b473cd525940417db5d4ec0f31d9a4
sr <waldnerds@gmail.com>
2023-02-19 20:16:07 +0100

    notification on copy staking address

--- src/assets/js/copy-content.js
+++ src/assets/js/copy-content.js
@@ -1,9 +1,10 @@
-  const copyContent = async () => {
-    let text = document.getElementById('validatorNode').value;
-    try {
-      await navigator.clipboard.writeText(text);
-      console.log('Content copied to clipboard');
-    } catch (err) {
-      console.error('Failed to copy: ', err);
-    }
+const copyContent = async () => {
+  let text = document.getElementById('validatorNode').value;
+  Notiflix.Notify.success("Staking address copied.")
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('Content copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy: ', err);
   }
+}
