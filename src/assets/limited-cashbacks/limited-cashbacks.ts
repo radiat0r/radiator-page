2258c8420ae3eac900857c75fbecabac0658de4d
sr <waldnerds@gmail.com>
2023-03-12 13:29:32 +0100

    use https url for limited cashbacks

--- src/assets/limited-cashbacks/limited-cashbacks.ts
+++ src/assets/limited-cashbacks/limited-cashbacks.ts
@@ -12,7 +12,7 @@ export interface LimitedCashback {
 export class LimitedCashbacksService {
     static loadLimitedCashbacks(): Promise<LimitedCashbacks> {
         return new Promise((resolve, reject) => {
-            fetch("http://a8c0085.online-server.cloud:5742/limited-cashbacks")
+            fetch("https://cashback.radiator.run/limited-cashbacks")
             //fetch("http://127.0.0.1:5742/limited-cashbacks")
                 .then(response => {
                     console.log(response)
