7db734ae4fe44582841f94bcb1c516692e3a64a7
sr <waldnerds@gmail.com>
2023-03-28 00:00:43 +0200

    reorder cashbacks new to old

--- src/components/rdt-wallet/cashbacks.ts
+++ src/components/rdt-wallet/cashbacks.ts
@@ -13,49 +13,56 @@ export type CashbackConfig = {
 
 export const Cashbacks = [
     {
-        project: 'Shardeez',
+        project: 'CerberRADS',
+        maxCashback: 20,
+        limitRdt: 666,
+        vikingland: 'https://www.vikingland.io/collection/CerberRADS',
+        telegram: 'https://t.me/radix_radiator/5556',
+        calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
+            if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
+                return config.maxCashback
+            } else {
+                return null
+            }
+        },
+    },
+    {
+        project: 'Radix Ratz',
+        maxCashback: 30,
+        limitRdt: 500,
+        vikingland: 'https://www.vikingland.io/collection/Radix%20Ratz',
+        telegram: 'https://t.me/radix_radiator/4886',
+        limitedProject: 'vikingland_radixratz',
+        calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
+            if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
+                return config.maxCashback
+            } else {
+                return null
+            }
+        },
+    },
+    {
+        project: 'Horrible Heads',
         maxCashback: 25,
-        limitRdt: 150,
-        limitNordicStake: 1000,
-        vikingland: 'https://www.vikingland.net/collection/Shardeez',
-        telegram: 'https://t.me/radix_radiator/1249',
+        limitRdt: 500,
+        vikingland: 'https://www.vikingland.io/collection/Horrible%20Heads',
+        telegram: 'https://t.me/radix_radiator/4571',
+        limitedProject: 'vikingland_horribleheads',
         calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
             if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
-                if (wallet.staked_at_nordic >= config.limitNordicStake) {
-                    return config.maxCashback
-                } else {
-                    return 20
-                }
+                return config.maxCashback
             } else {
                 return null
             }
         },
     },
     {
-        project: 'Mutant Cat Society',
+        project: 'Roidettes',
         maxCashback: 20,
-        limitRdt: 150,
-        limitNordicStake: 1000,
-        vikingland: 'https://www.vikingland.net/collection/Mutant%20Cat%20Society',
-        telegram: 'https://t.me/radix_radiator/1886',
-        calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
-            if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
-                if (wallet.staked_at_nordic >= config.limitNordicStake) {
-                    return config.maxCashback
-                } else {
-                    return 15
-                }
-            } else {
-                return null
-            }
-        },
-    },
-    {
-        project: 'Mystic Tiger Brotherhood',
-        maxCashback: 20,
-        limitRdt: 150,
-        vikingland: 'https://www.vikingland.net/collection/Mystic%20Tigers%20Brotherhood',
-        telegram: 'https://t.me/radix_radiator/1654',
+        limitRdt: 300,
+        vikingland: 'https://www.vikingland.io/collection/Roidettes',
+        telegram: 'https://t.me/radix_radiator/3605',
+        limitedProject: 'vikingland_roidettes',
         calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
             if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
                 return config.maxCashback
@@ -84,12 +91,11 @@ export const Cashbacks = [
         },
     },
     {
-        project: 'Roidettes',
+        project: 'Mystic Tiger Brotherhood',
         maxCashback: 20,
-        limitRdt: 300,
-        vikingland: 'https://www.vikingland.io/collection/Roidettes',
-        telegram: 'https://t.me/radix_radiator/3605',
-        limitedProject: 'vikingland_roidettes',
+        limitRdt: 150,
+        vikingland: 'https://www.vikingland.net/collection/Mystic%20Tigers%20Brotherhood',
+        telegram: 'https://t.me/radix_radiator/1654',
         calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
             if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
                 return config.maxCashback
@@ -99,47 +105,41 @@ export const Cashbacks = [
         },
     },
     {
-        project: 'Horrible Heads',
+        project: 'Mutant Cat Society',
+        maxCashback: 20,
+        limitRdt: 150,
+        limitNordicStake: 1000,
+        vikingland: 'https://www.vikingland.net/collection/Mutant%20Cat%20Society',
+        telegram: 'https://t.me/radix_radiator/1886',
+        calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
+            if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
+                if (wallet.staked_at_nordic >= config.limitNordicStake) {
+                    return config.maxCashback
+                } else {
+                    return 15
+                }
+            } else {
+                return null
+            }
+        },
+    },
+    {
+        project: 'Shardeez',
         maxCashback: 25,
-        limitRdt: 500,
-        vikingland: 'https://www.vikingland.io/collection/Horrible%20Heads',
-        telegram: 'https://t.me/radix_radiator/4571',
-        limitedProject: 'vikingland_horribleheads',
+        limitRdt: 150,
+        limitNordicStake: 1000,
+        vikingland: 'https://www.vikingland.net/collection/Shardeez',
+        telegram: 'https://t.me/radix_radiator/1249',
         calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
             if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
-                return config.maxCashback
+                if (wallet.staked_at_nordic >= config.limitNordicStake) {
+                    return config.maxCashback
+                } else {
+                    return 20
+                }
             } else {
                 return null
             }
         },
     },
-    {
-        project: 'Radix Ratz',
-        maxCashback: 30,
-        limitRdt: 500,
-        vikingland: 'https://www.vikingland.io/collection/Radix%20Ratz',
-        telegram: 'https://t.me/radix_radiator/4886',
-        limitedProject: 'vikingland_radixratz',
-        calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
-            if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
-                return config.maxCashback
-            } else {
-                return null
-            }
-        },
-    },
-    {
-        project: 'CerberRADS',
-        maxCashback: 20,
-        limitRdt: 666,
-        vikingland: 'https://www.vikingland.io/collection/CerberRADS',
-        telegram: 'https://t.me/radix_radiator/5556',
-        calcCashbackBenefit: (config: CashbackConfig, wallet: Wallet): number => {
-            if (wallet.rdt >= config.limitRdt && wallet.rdt_7_days_ago >= config.limitRdt) {
-                return config.maxCashback
-            } else {
-                return null
-            }
-        },
-    }
 ]
