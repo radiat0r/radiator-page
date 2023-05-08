db377453451edb72f699c42f0d1383815a1b06a0
Martin Kötzing <mkoetzing@users.noreply.github.com>
2023-05-09 00:21:58 +0200

    Update contactform.js

--- src/assets/js/contactform.js
+++ src/assets/js/contactform.js
@@ -6,9 +6,9 @@ const contactformular = async () => {
 
   // smtpjs settings
   var emailConfig = {
-    SecureToken: "7779d413-d224-468f-936b-9ac5e0b8086c",
+    SecureToken: "7ee76e43-fcd4-495f-a5c0-2b32cff5223a",
     To: "info@radiator.run",
-    From: email,
+    From: "radixradiator@gmail.com",
     Subject: "Neue Kontaktanfrage",
     Body: "Name: " + name + "<br>" + "E-Mail: " + email + "<br>" + "Nachricht: " + message
   };
@@ -24,3 +24,8 @@ const contactformular = async () => {
     }
   );
 }
+
+// working -> this info has to be deleted before publishing
+// API FE3A70B50E7D286787A5BBFB8E58E943B2ABC91D0CE893D8AFBB4B3F4184E8576EE7F48A1A5BD4DB675ECB7070EF89DC
+// PW 8EA3FE37E39BDAE62D1ED34872DFCF2C6297
+// TOKEN PORT 2525 7ee76e43-fcd4-495f-a5c0-2b32cff5223a
