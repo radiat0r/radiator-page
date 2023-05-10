e500ed56b9543cc39f12485286fcd5c6b5c67563
Martin Kötzing <mkoetzing@users.noreply.github.com>
2023-05-10 23:27:59 +0200

    wip

--- src/assets/js/contactform.js
+++ src/assets/js/contactform.js
@@ -8,24 +8,59 @@ const contactformular = async () => {
   var emailConfig = {
     SecureToken: "7ee76e43-fcd4-495f-a5c0-2b32cff5223a",
     To: "info@radiator.run",
-    From: "radixradiator@gmail.com",
+    From: "info@radiator.run",
     Subject: "Neue Kontaktanfrage",
     Body: "Name: " + name + "<br>" + "E-Mail: " + email + "<br>" + "Nachricht: " + message
   };
 
   // send email with smtpjs
   Email.send(emailConfig).then(
-    function (response) {
-      Notiflix.Notify.success("E-Mail sent successfully!")
-      form.reset();
-    },
-    function (error) {
-      Notiflix.Notify.error("E-Mail could not be sent!", error)
+    (response) => { 
+      console.log(response);
+      if (response == "OK") {
+        Notiflix.Notify.success("E-Mail sent successfully!")
+        form.reset();
+      }
+    }
+  )
+  .catch(
+    (response) => { 
+      console.log(response);
+      Notiflix.Notify.failure(response)
     }
   );
 }
 
-// working -> this info has to be deleted before publishing
-// API FE3A70B50E7D286787A5BBFB8E58E943B2ABC91D0CE893D8AFBB4B3F4184E8576EE7F48A1A5BD4DB675ECB7070EF89DC
-// PW 8EA3FE37E39BDAE62D1ED34872DFCF2C6297
-// TOKEN PORT 2525 7ee76e43-fcd4-495f-a5c0-2b32cff5223a
+
+
+/* SmtpJS.com - v3.0.0 
+var Email = {
+  send: function(a) {
+      return new Promise(function(n, e) {
+          a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
+          var t = JSON.stringify(a);
+          Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function(e) {
+              n(e)
+          })
+      })
+  },
+  ajaxPost: function(e, n, t) {
+      var a = Email.createCORSRequest("POST", e);
+      a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function() {
+          var e = a.responseText;
+          null != t && t(e)
+      }, a.send(n)
+  },
+  ajax: function(e, n) {
+      var t = Email.createCORSRequest("GET", e);
+      t.onload = function() {
+          var e = t.responseText;
+          null != n && n(e)
+      }, t.send()
+  },
+  createCORSRequest: function(e, n) {
+      var t = new XMLHttpRequest;
+      return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t
+  }
+};
+*/
